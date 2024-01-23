3731f5f1da413194b1a9fa74c2fc71c2c232fe5d
DevinTan4 <vninecoc1@gmail.com>
2024-01-23 23:25:49 +0700

    feat: create cart

--- scripts/pages/amazon.js
+++ scripts/pages/amazon.js
@@ -46,7 +46,9 @@ products.forEach((product) => {
         Added
       </div>
 
-      <button class="add-to-cart-button button-primary">
+      <button class="add-to-cart-button button-primary" data-product-id = "${
+        product.id
+      }">
         Add to Cart
       </button>
     </div>
@@ -54,3 +56,29 @@ products.forEach((product) => {
 });
 
 document.querySelector(".products-grid").innerHTML = productsHTML;
+
+document.querySelectorAll(".add-to-cart-button").forEach((button) => {
+  button.addEventListener("click", () => {
+    // Dataset use for taking all the data from the button in the data atribut
+    const productId = button.dataset.productId;
+
+    let matchingItem;
+
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
+    });
+
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: 1,
+      });
+    }
+
+    console.log(cart);
+  });
+});
