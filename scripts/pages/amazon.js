205d60fd4fc9d15331d1a13cbac1b2533c2cbd69
DevinTan4 <vninecoc1@gmail.com>
2024-01-25 20:07:26 +0700

    feat: add quantity using quantity selector

--- scripts/pages/amazon.js
+++ scripts/pages/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product) => {
       </div>
 
       <div class="product-quantity-container">
-        <select>
+        <select class="quantity-selector-${product.id}">
           <option selected value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
@@ -70,12 +70,18 @@ document.querySelectorAll(".add-to-cart-button").forEach((button) => {
       }
     });
 
+    // Quantity selector
+    const quantitySelector = document.querySelector(
+      `.quantity-selector-${productId}`
+    );
+    const quantity = Number(quantitySelector.value);
+
     if (matchingItem) {
-      matchingItem.quantity += 1;
+      matchingItem.quantity += quantity;
     } else {
       cart.push({
         productId: productId,
-        quantity: 1,
+        quantity: quantity,
       });
     }
 
