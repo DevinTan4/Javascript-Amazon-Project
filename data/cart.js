f2c8a6d30aa35fec9792f8ed5049014f23c9e4a4
DevinTan4 <vninecoc1@gmail.com>
2024-02-18 23:12:49 +0700

    feat: save cart to localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,14 +1,22 @@
-export let cart = [
-  {
-    // Default value
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2,
-  },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1,
-  },
-];
+export let cart = JSON.parse(localStorage.getItem("cart"));
+
+if (!cart) {
+  cart = [
+    {
+      // Default value
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2,
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 1,
+    },
+  ];
+}
+
+const saveToStorage = () => {
+  localStorage.setItem("cart", JSON.stringify(cart));
+};
 
 // Add to cart function
 export function addToCart(productId) {
@@ -34,6 +42,8 @@ export function addToCart(productId) {
       quantity,
     });
   }
+
+  saveToStorage();
 }
 
 export const removeFromCart = (productId) => {
@@ -46,4 +56,6 @@ export const removeFromCart = (productId) => {
   });
 
   cart = newCart;
+
+  saveToStorage();
 };
